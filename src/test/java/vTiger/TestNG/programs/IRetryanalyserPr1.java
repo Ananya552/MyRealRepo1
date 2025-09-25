35af6f047f7e9afba660af20e37471de5ad2eb9a
Ananya <Ananya@LAPTOP-2UVM9916>
2025-09-25 09:25:35 +0530

    ABCD

--- src/test/java/vTiger/TestNG/programs/IRetryanalyserPr1.java
+++ src/test/java/vTiger/TestNG/programs/IRetryanalyserPr1.java
@@ -3,7 +3,6 @@ package vTiger.TestNG.programs;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-
 public class IRetryanalyserPr1 {
 	@Test(retryAnalyzer = vTiger.Generic.Utilities.IRetryAnalyserImpl.class)
 	public void meth1() {
